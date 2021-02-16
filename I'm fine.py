4857facc5d4fa296a93900893d240c5fcf4edf35
Lencof <71154457+Lencof@users.noreply.github.com>
2021-02-16 13:36:49 +0300

    Update I'm fine.py

--- I'm fine.py
+++ I'm fine.py
@@ -1,1 +1,7 @@
+# __Author__ __Lencof__
+# I'm fine.py
 
+# I put double commas so that the text does not stand out
+print("I'm fine") # your text
+print("I'm fine") # your text
+print("I'm fine") # your text
