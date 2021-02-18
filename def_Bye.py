75ba2536130d1fa46c89e6731452b2033c000eb4
Lencof <71154457+Lencof@users.noreply.github.com>
2021-02-18 04:20:30 +0300

    Update def_Bye.py

--- def_Bye.py
+++ def_Bye.py
@@ -1,2 +1,16 @@
 # __Author__ __Lencof__
 # def_Bye.py
+
+def Dior(): # create def Dior()
+    print('Bye!') # your text
+    print('Bye!') # your text
+    print('Bye!') # your text
+    
+def Jo(): # create def Jo()
+    print('No bye!') # your text
+    print('No bye!') # your text
+    print('No bye!') # your text
+    
+Dior() # close
+Jo() # close
+# if we do not close with parentheses it will not work for us, Dior() and Jo()
