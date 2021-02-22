cfbf7b8d7b7bc1243974201797d1d2feafaa68d9
Lencof <71154457+Lencof@users.noreply.github.com>
2021-02-22 08:05:15 +0300

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -14,3 +14,7 @@ print('Hello Worlds') # your text
 print("Hello Worlds") # your text
 print("Hello Worlds") # your text
 print("Hello worlds") # your text
+
+print('Hello Worlds') # your text
+print('Hello Worlds') # your text
+print('Hello Wrolds') # your text
