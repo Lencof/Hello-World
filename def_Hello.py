a4b54abb59cc764bf8cd5b05f4afd43a4f586956
Lencof <71154457+Lencof@users.noreply.github.com>
2021-02-25 12:24:40 +0300

    Update def_Hello.py

--- def_Hello.py
+++ def_Hello.py
@@ -7,4 +7,8 @@ def Hello(): # create Hello
     print("Hello Worlds") # your text
     print("Hello Worlds") # your text
     
+    print('Hello Worlds') # your text
+    print('Hello worlds') # your text
+    print('Hello worlds') # your text
+    
 Hello() # close
