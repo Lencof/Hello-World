a03f4f2c563c2b07ac43ca1a9cacf662163b74f8
Lencof <71154457+Lencof@users.noreply.github.com>
2021-02-27 10:32:02 +0300

    Update def_Falcon.py

--- def_Falcon.py
+++ def_Falcon.py
@@ -1,2 +1,10 @@
 # __Author__ __Lencof__
 # def_Falcon.py
+
+# create def Falcon():
+def Falcon():
+    print('Falcon') # your text
+    print('Falcon') # your text
+    print('Falcon') # your text
+    
+Falcon() # close
