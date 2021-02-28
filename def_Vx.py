7bc51981eb6f80da5ced365027dd284df6253a86
Lencof <71154457+Lencof@users.noreply.github.com>
2021-02-28 10:57:21 +0300

    Update def_Vx.py

--- def_Vx.py
+++ def_Vx.py
@@ -1,1 +1,15 @@
+# __Author__ __Lencof__
+# def_Vx.py
 
+def Vx(): # creste def Vx():
+    print('Help me!') # your text
+    print('Help me!') # your text
+    print('Help me!') # your text
+    
+def Vs(): # create def Vs():
+    print('No thank') # your text
+    print('No thank') # your text
+    print('No thank') # your text
+    
+Vx() # close
+Vs() # close
