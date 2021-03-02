51e0d3ffe7220d7768dbbdc47d21a730fee767c4
Lencof <71154457+Lencof@users.noreply.github.com>
2021-03-02 13:10:19 +0300

    Create def_Bav.py

---
+++ def_Bav.py
@@ -0,0 +1,15 @@
+# __Author__ __Lencof__
+# def_Bav.py
+
+class Bav():
+    print('Bav')
+    print('Bav')
+    print('Bav')
+  
+class Bax():
+    print('Bax') 
+    print('Bax')
+    print('Bax')
+    
+Bav()
+Bax()
