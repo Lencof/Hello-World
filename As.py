d028653dd24da8bdbf058f300dc61cb11195c18e
Lencof <71154457+Lencof@users.noreply.github.com>
2021-03-05 09:10:10 +0300

    Update As.py

--- As.py
+++ As.py
@@ -1,2 +1,10 @@
 # __Author__ __Lencof__
 # As.py
+
+print("As") # your text
+print("As") # your text
+print("As") # your text
+
+print('As') # your text
+print('As') # your text
+print('As') # your text
