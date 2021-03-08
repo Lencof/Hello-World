82caaa8393366d71b3ea233d7931769bf7e68269
Lencof <71154457+Lencof@users.noreply.github.com>
2021-03-08 06:33:07 +0300

    Update see.py

--- see.py
+++ see.py
@@ -1,3 +1,14 @@
+# __Author__ __Lencof__
+# see.py
+
+print('see') # your text
+print('see') # your text
+print('see') # your text
+
+print('see') # your text
+print('see') # your text
+print('see') # your text
+
 print('see') # your text
 print('see') # your text
 print('see') # your text
