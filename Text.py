0781bbc7ab90b62944e8b1ef6598f9270178a1cf
Lencof <71154457+Lencof@users.noreply.github.com>
2021-03-09 07:24:17 +0300

    Create Text.py

---
+++ Text.py
@@ -0,0 +1,12 @@
+# __Author__ __Lencof__
+# Text.py
+
+# create dictionary Text
+Text = {
+  'Hi',   # your text
+  'Hello',   # your text
+  'Good',    # your text
+  'Bye!'     # your text
+}
+
+print(Text) # print() deduces
