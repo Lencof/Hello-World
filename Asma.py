cf2c3253687ae9af5069792e82beb9c89ed802fe
Lencof <71154457+Lencof@users.noreply.github.com>
2021-03-10 07:57:39 +0300

    Create Asma.py

---
+++ Asma.py
@@ -0,0 +1,14 @@
+# __Author__ __Lencof__
+# Asma.py
+
+print('Asma') # your text
+print('Asma') # your text
+print('Asma') # your text
+
+print('Asma') # your text
+print('Asma') # your text
+print('Asma') # your text
+
+print('Asma') # your text
+print('Asma') # your text
+print('Asma') # your text
