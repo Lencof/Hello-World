1fca6f30b8265d996346af27bb7cf337ad41b4b1
Lencof <71154457+Lencof@users.noreply.github.com>
2021-03-11 07:56:23 +0300

    Create No.py

---
+++ No.py
@@ -0,0 +1,14 @@
+# __Author__ __Lencof__
+# No.py
+
+print('No') # your text
+print('No') # your text
+print('No') # your text
+
+print('No') # your text
+print('No') # your text
+print('No') # your text
+
+print('No') # your text
+print('No') # your text
+print('No') # your text
