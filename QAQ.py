00bf78f96c9bdb9f3e5ea5c37df7c7dea2e784b7
Lencof <71154457+Lencof@users.noreply.github.com>
2021-03-14 13:25:41 +0300

    Create QAQ.py

---
+++ QAQ.py
@@ -0,0 +1,14 @@
+# __Author__ __Lencof__
+# QAQ.py
+
+print('QAQ') # your text
+print('QAQ') # your text
+print('QAQ') # your text
+
+print('QAQ') # your text
+print('QAQ') # your text
+print('QAQ') # your text
+
+print('QAQ') # your text
+print('QAQ') # your text
+print('QAQ') # your text
