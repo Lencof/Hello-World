232b8837400b2ff909c418cff905866d607dc064
Lencof <71154457+Lencof@users.noreply.github.com>
2021-03-14 13:28:02 +0300

    Create BAX.py

---
+++ BAX.py
@@ -0,0 +1,14 @@
+# __Author__ __Lencof__
+# BAX.py
+
+print('BAX') # your text
+print('BAX') # your text
+print('BAX') # your text
+
+print('BAX') # your text
+print('BAX') # your text
+print('BAX') # your text
+
+print('BAX') # your text
+print('BAX') # your text
+print('BAX') # your text
