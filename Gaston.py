9fa689656d296754712d3beeb911b5df8cfc54dc
Lencof <71154457+Lencof@users.noreply.github.com>
2021-03-16 02:58:21 +0300

    Create Gaston.py

---
+++ Gaston.py
@@ -0,0 +1,14 @@
+# __Author__ __Lencof__
+# Gaston.py
+
+print('Gaston') # your text
+print('Gaston') # your text
+print('Gaston') # your text
+
+print('Gaston') # your text
+print('Gaston') # your text
+print('Gaston') # your text
+
+print('Gaston') # your text
+print('Gaston') # your text
+print('Gaston') # your text
