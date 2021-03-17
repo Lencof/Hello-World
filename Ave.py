5fdf172e07f82b1aa935c1bdeb9ff315a8a3a651
Lencof <71154457+Lencof@users.noreply.github.com>
2021-03-18 02:43:30 +0300

    Create Ave.py

---
+++ Ave.py
@@ -0,0 +1,14 @@
+# __Author__ __Lencof__
+# Ave.py
+
+print('Ave') # your text
+print('Ave') # your text
+print('Ave') # your text
+
+print('Ave') # your text
+print('Ave') # your text
+print('Ave') # your text
+ 
+print('Ave') # your text
+print('Ave') # your text
+print('Ave') # your text
