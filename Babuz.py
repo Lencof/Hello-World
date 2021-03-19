a635a939f414dd656f3248387c7509ef13339719
Lencof <71154457+Lencof@users.noreply.github.com>
2021-03-19 09:00:49 +0600

    Create Babuz.py

---
+++ Babuz.py
@@ -0,0 +1,14 @@
+# __Author__ __Lencof__
+# Babuz.py
+
+print('Babuz') # your text
+print('Babuz') # your text
+print('Babuz') # your text
+
+print('Babuz') # your text
+print('Babuz') # your text
+print('Babuz') # your text
+
+print('Babuz') # your text
+print('Babuz') # your text
+print('Babuz') # your text
