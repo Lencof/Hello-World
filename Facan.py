b0f6d8449f38b0101d5d3a049f8acd13d50b6f31
Lencof <71154457+Lencof@users.noreply.github.com>
2021-04-03 07:48:23 +0300

    Create Facan.py

---
+++ Facan.py
@@ -0,0 +1,38 @@
+# __Author__ __Lencof__
+# Facan.py
+
+print('Facan') # your text
+print('Facan') # your text
+print('Facan') # your text
+
+print('Facan') # your text
+print('Facan') # your text
+print('Facan') # your text
+
+print('Facan') # your text
+print('Facan') # your text
+print('Facan') # your text
+
+print('Facan') # your text
+print('Facan') # your text
+print('Facan') # your text
+
+print('Facan') # your text
+print('Facan') # your text
+print('Facan') # your text
+
+print('Facan') # your text
+print('Facan') # your text
+print('Facan') # your text
+
+print('Facan') # your text
+print('Facan') # your text
+print('Facan') # your text
+
+print('Facan') # your text
+print('Facan') # your text
+print('Facan') # your text
+
+print('Facan') # your text
+print('Facan') # your text
+print('Facan') # your text
