83174aae171b8bb432fb3c3a6358fb20b4f87c85
Lencof <71154457+Lencof@users.noreply.github.com>
2021-04-09 07:12:03 +0300

    Update Babuz.py

--- Babuz.py
+++ Babuz.py
@@ -1,6 +1,32 @@
 # __Author__ __Lencof__
 # Babuz.py
 
+import os
+
+print('Babuz') # your text
+print('Babuz') # your text
+print('Babuz') # your text
+
+print('Babuz') # your text
+print('Babuz') # your text
+print('Babuz') # your text
+
+print('Babuz') # your text
+print('Babuz') # your text
+print('Babuz') # your text
+
+print('Babuz') # your text
+print('Babuz') # your text
+print('Babuz') # your text
+
+print('Babuz') # your text
+print('Babuz') # your text
+print('Babuz') # your text
+
+print('Babuz') # your text
+print('Babuz') # your text
+print('Babuz') # your text
+
 print('Babuz') # your text
 print('Babuz') # your text
 print('Babuz') # your text
