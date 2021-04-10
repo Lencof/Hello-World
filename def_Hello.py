95493d0c0db05e043081613169e97d828f3c371a
Lencof <71154457+Lencof@users.noreply.github.com>
2021-04-10 05:11:27 +0300

    Update def_Hello.py

--- def_Hello.py
+++ def_Hello.py
@@ -1,3 +1,8 @@
+# __Author__ __Lencof__
+# def_Hello.py
+
+import os
+
 def Hello(): # create Hello
     print('Hello Worlds') # your text
     print('Hello Worlds') # your text
@@ -11,4 +16,28 @@ def Hello(): # create Hello
     print('Hello worlds') # your text
     print('Hello worlds') # your text
     
+    print('Hello Worlds') # your text
+    print('Hello Worlds') # your text
+    print('Hello Worlds') # your text
+    
+    print("Hello Worlds") # your text 
+    print("Hello Worlds") # your text
+    print("Hello Worlds") # your text
+    
+    print('Hello Worlds') # your text
+    print('Hello worlds') # your text
+    print('Hello worlds') # your text
+    
+    print('Hello Worlds') # your text
+    print('Hello Worlds') # your text
+    print('Hello Worlds') # your text
+    
+    print("Hello Worlds") # your text 
+    print("Hello Worlds") # your text
+    print("Hello Worlds") # your text
+    
+    print('Hello Worlds') # your text
+    print('Hello worlds') # your text
+    print('Hello worlds') # your text
+
 Hello() # close
