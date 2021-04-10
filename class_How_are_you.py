4a8816939157406694dc5a29139fa886120e8366
Lencof <71154457+Lencof@users.noreply.github.com>
2021-04-10 05:08:59 +0300

    Update class_How_are_you.py

--- class_How_are_you.py
+++ class_How_are_you.py
@@ -1,6 +1,8 @@
 # __Author__ __Lencof__
 # class_How_are_you.py
 
+import os
+
 class How_are_you(): # create class How_are_you():
     print('How are you?') # your text
     print('How are you?') # your text 
