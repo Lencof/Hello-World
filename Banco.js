4448fea777f72a90a3b1178d69a3d04d51cb3515
Lencof <71154457+Lencof@users.noreply.github.com>
2021-04-22 04:03:51 +0300

    Create Banco.js

---
+++ Banco.js
@@ -0,0 +1,38 @@
+// __Author__ __Lencof__
+// Banco.js
+
+console.log('Banco') // your text
+console.log('Banco') // your text
+console.log('Banco') // your text
+
+console.log('Banco') // your text
+console.log('Banco') // your text
+console.log('Banco') // your text
+
+console.log('Banco') // your text
+console.log('Banco') // your text
+console.log('Banco') // your text
+
+console.log('Banco') // your text
+console.log('Banco') // your text
+console.log('Banco') // your text
+
+console.log('Banco') // your text
+console.log('Banco') // your text
+console.log('Banco') // your text
+
+console.log('Banco') // your text
+console.log('Banco') // your text
+console.log('Banco') // your text
+
+console.log('Banco') // your text
+console.log('Banco') // your text
+console.log('Banco') // your text
+
+console.log('Banco') // your text
+console.log('Banco') // your text
+console.log('Banco') // your text
+
+console.log('Banco') // your text
+console.log('Banco') // your text
+console.log('Banco') // your text
