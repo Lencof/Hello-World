f3fb434329690c01fb46790404867a4968e504c6
Lencof <71154457+Lencof@users.noreply.github.com>
2021-04-22 04:08:33 +0300

    Create Plot.js

---
+++ Plot.js
@@ -0,0 +1,38 @@
+// __Author__ __Lencof__
+// Plot.js
+
+console.log('Plot') // your text
+console.log('Plot') // your text
+console.log('Plot') // your text
+
+console.log('Plot') // your text
+console.log('Plot') // your text
+console.log('Plot') // your text
+
+console.log('Plot') // your text
+console.log('Plot') // your text
+console.log('Plot') // your text
+
+console.log('Plot') // your text
+console.log('Plot') // your text
+console.log('Plot') // your text
+
+console.log('Plot') // your text
+console.log('Plot') // your text
+console.log('Plot') // your text
+
+console.log('Plot') // your text
+console.log('Plot') // your text
+console.log('Plot') // your text
+
+console.log('Plot') // your text
+console.log('Plot') // your text
+console.log('Plot') // your text
+
+console.log('Plot') // your text
+console.log('Plot') // your text
+console.log('Plot') // your text
+
+console.log('Plot') // your text
+console.log('Plot') // your text
+console.log('Plot') // your text
