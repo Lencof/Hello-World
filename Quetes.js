dd4a4587aa40eff1c5a8fa42d2741ee803f34f23
Lencof <71154457+Lencof@users.noreply.github.com>
2021-04-23 03:07:01 +0300

    Create Quetes.js

---
+++ Quetes.js
@@ -0,0 +1,38 @@
+// __Author__ __Lencof__
+// Quetes.js
+
+console.log('Quetes') // your text
+console.log('Quetes') // your text
+console.log('Quetes') // your text
+
+console.log('Quetes') // your text
+console.log('Quetes') // your text
+console.log('Quetes') // your text
+
+console.log('Quetes') // your text
+console.log('Quetes') // your text
+console.log('Quetes') // your text
+
+console.log('Quetes') // your text
+console.log('Quetes') // your text
+console.log('Quetes') // your text
+
+console.log('Quetes') // your text
+console.log('Quetes') // your text
+console.log('Quetes') // your text
+
+console.log('Quetes') // your text
+console.log('Quetes') // your text
+console.log('Quetes') // your text
+
+console.log('Quetes') // your text
+console.log('Quetes') // your text
+console.log('Quetes') // your text
+
+console.log('Quetes') // your text
+console.log('Quetes') // your text
+console.log('Quetes') // your text
+
+console.log('Quetes') // your text
+console.log('Quetes') // your text
+console.log('Quetes') // your text
