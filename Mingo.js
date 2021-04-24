354547e74d1b912bba4e81fb80f5dd610437154c
Lencof <71154457+Lencof@users.noreply.github.com>
2021-04-24 12:49:06 +0300

    Create Mingo.js

---
+++ Mingo.js
@@ -0,0 +1,38 @@
+// __Author__ __Lencof__
+// Mingo.js
+
+console.log('Mingo') // your text
+console.log('Mingo') // your text
+console.log('Mingo') // your text
+
+console.log('Mingo') // your text
+console.log('Mingo') // your text
+console.log('Mingo') // your text
+
+console.log('Mingo') // your text
+console.log('Mingo') // your text
+console.log('Mingo') // your text
+
+console.log('Mingo') // your text
+console.log('Mingo') // your text
+console.log('Mingo') // your text
+
+console.log('Mingo') // your text
+console.log('Mingo') // your text
+console.log('Mingo') // your text
+
+console.log('Mingo') // your text
+console.log('Mingo') // your text
+console.log('Mingo') // your text
+
+console.log('Mingo') // your text
+console.log('Mingo') // your text
+console.log('Mingo') // your text
+
+console.log('Mingo') // your text
+console.log('Mingo') // your text
+console.log('Mingo') // your text
+
+console.log('Mingo') // your text
+console.log('Mingo') // your text
+console.log('Mingo') // your text
