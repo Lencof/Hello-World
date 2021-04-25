cfdddc455bd46b937d5f201db048d69bb417cbc6
Lencof <71154457+Lencof@users.noreply.github.com>
2021-04-25 09:33:28 +0300

    Create Guza.js

---
+++ Guza.js
@@ -0,0 +1,38 @@
+// __Author__ __Lencof__
+// Guza.js
+
+console.log('Guza') // your text
+console.log('Guza') // your text
+console.log('Guza') // your text
+
+console.log('Guza') // your text
+console.log('Guza') // your text
+console.log('Guza') // your text
+
+console.log('Guza') // your text
+console.log('Guza') // your text
+console.log('Guza') // your text
+
+console.log('Guza') // your text
+console.log('Guza') // your text
+console.log('Guza') // your text
+
+console.log('Guza') // your text
+console.log('Guza') // your text
+console.log('Guza') // your text
+
+console.log('Guza') // your text
+console.log('Guza') // your text
+console.log('Guza') // your text
+
+console.log('Guza') // your text
+console.log('Guza') // your text
+console.log('Guza') // your text
+
+console.log('Guza') // your text
+console.log('Guza') // your text
+console.log('Guza') // your text
+
+console.log('Guza') // your text
+console.log('Guza') // your text
+console.log('Guza') // your text
