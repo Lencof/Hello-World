839ebe870b3f356dcf0fff20bf77db07cc1e0864
Lencof <71154457+Lencof@users.noreply.github.com>
2021-04-26 03:58:09 +0300

    Create Clab.js

---
+++ Clab.js
@@ -0,0 +1,38 @@
+// __Author__ __Lencof__
+// Clab.js
+
+console.log('Clab') // your text
+console.log('Clab') // your text
+console.log('Clab') // your text
+
+console.log('Clab') // your text
+console.log('Clab') // your text
+console.log('Clab') // your text
+
+console.log('Clab') // your text
+console.log('Clab') // your text
+console.log('Clab') // your text
+
+console.log('Clab') // your text
+console.log('Clab') // your text
+console.log('Clab') // your text
+
+console.log('Clab') // your text
+console.log('Clab') // your text
+console.log('Clab') // your text
+
+console.log('Clab') // your text
+console.log('Clab') // your text
+console.log('Clab') // your text
+
+console.log('Clab') // your text
+console.log('Clab') // your text
+console.log('Clab') // your text
+
+console.log('Clab') // your text
+console.log('Clab') // your text
+console.log('Clab') // your text
+
+console.log('Clab') // your text
+console.log('Clab') // your text
+console.log('Clab') // your text
