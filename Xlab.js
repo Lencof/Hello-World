b898fbdb22f0b45599cfea4aad800fc681430088
Lencof <71154457+Lencof@users.noreply.github.com>
2021-04-26 03:59:58 +0300

    Create Xlab.js

---
+++ Xlab.js
@@ -0,0 +1,38 @@
+// __Author__ __Lencof__
+// Xlab.js
+
+cosnole.log('Xlab') // your text
+cosnole.log('Xlab') // your text
+cosnole.log('Xlab') // your text
+
+cosnole.log('Xlab') // your text
+cosnole.log('Xlab') // your text
+cosnole.log('Xlab') // your text
+
+cosnole.log('Xlab') // your text
+cosnole.log('Xlab') // your text
+cosnole.log('Xlab') // your text
+
+cosnole.log('Xlab') // your text
+cosnole.log('Xlab') // your text
+cosnole.log('Xlab') // your text
+
+cosnole.log('Xlab') // your text
+cosnole.log('Xlab') // your text
+cosnole.log('Xlab') // your text
+
+cosnole.log('Xlab') // your text
+cosnole.log('Xlab') // your text
+cosnole.log('Xlab') // your text
+
+cosnole.log('Xlab') // your text
+cosnole.log('Xlab') // your text
+cosnole.log('Xlab') // your text
+
+cosnole.log('Xlab') // your text
+cosnole.log('Xlab') // your text
+cosnole.log('Xlab') // your text
+
+cosnole.log('Xlab') // your text
+cosnole.log('Xlab') // your text
+cosnole.log('Xlab') // your text
