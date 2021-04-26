e786034aa2c4fd3d8adeeb8e177a0acf88379905
Lencof <71154457+Lencof@users.noreply.github.com>
2021-04-26 04:01:24 +0300

    Create Rebol.js

---
+++ Rebol.js
@@ -0,0 +1,38 @@
+// __Author__ __Lencof__
+// Rebol.js
+
+console.log('Rebol') // your text
+console.log('Rebol') // your text
+console.log('Rebol') // your text
+
+console.log('Rebol') // your text
+console.log('Rebol') // your text
+console.log('Rebol') // your text
+
+console.log('Rebol') // your text
+console.log('Rebol') // your text
+console.log('Rebol') // your text
+
+console.log('Rebol') // your text
+console.log('Rebol') // your text
+console.log('Rebol') // your text
+
+console.log('Rebol') // your text
+console.log('Rebol') // your text
+console.log('Rebol') // your text
+
+console.log('Rebol') // your text
+console.log('Rebol') // your text
+console.log('Rebol') // your text
+
+console.log('Rebol') // your text
+console.log('Rebol') // your text
+console.log('Rebol') // your text
+
+console.log('Rebol') // your text
+console.log('Rebol') // your text
+console.log('Rebol') // your text
+
+console.log('Rebol') // your text
+console.log('Rebol') // your text
+console.log('Rebol') // your text
