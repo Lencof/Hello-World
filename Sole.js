4afd20c17e35a3ad8c3e9f9c9fc32600b6834489
Lencof <71154457+Lencof@users.noreply.github.com>
2021-05-06 10:29:08 +0300

    Create Sole.js

---
+++ Sole.js
@@ -0,0 +1,38 @@
+// __Author__ __Lencof__
+// Sole.js
+
+console.log('Sole') // your text
+console.log('Sole') // your text
+console.log('Sole') // your text
+
+console.log('Sole') // your text
+console.log('Sole') // your text
+console.log('Sole') // your text
+
+console.log('Sole') // your text
+console.log('Sole') // your text
+console.log('Sole') // your text
+
+console.log('Sole') // your text
+console.log('Sole') // your text
+console.log('Sole') // your text
+
+console.log('Sole') // your text
+console.log('Sole') // your text
+console.log('Sole') // your text
+
+console.log('Sole') // your text
+console.log('Sole') // your text
+console.log('Sole') // your text
+
+console.log('Sole') // your text
+console.log('Sole') // your text
+console.log('Sole') // your text
+
+console.log('Sole') // your text
+console.log('Sole') // your text
+console.log('Sole') // your text
+
+console.log('Sole') // your text
+console.log('Sole') // your text
+console.log('Sole') // your text
