d39a091616fb935868103edbcc7bb1ae56ba7208
Lencof <71154457+Lencof@users.noreply.github.com>
2021-05-06 10:27:13 +0300

    Create Axe.js

---
+++ Axe.js
@@ -0,0 +1,38 @@
+// __Author__ __Lencof__
+// Axe.js
+
+console.log('Axe') // your text
+console.log('Axe') // your text
+console.log('Axe') // your text
+
+console.log('Axe') // your text
+console.log('Axe') // your text
+console.log('Axe') // your text
+
+console.log('Axe') // your text
+console.log('Axe') // your text
+console.log('Axe') // your text
+
+console.log('Axe') // your text
+console.log('Axe') // your text
+console.log('Axe') // your text
+
+console.log('Axe') // your text
+console.log('Axe') // your text
+console.log('Axe') // your text
+
+console.log('Axe') // your text
+console.log('Axe') // your text
+console.log('Axe') // your text
+
+console.log('Axe') // your text
+console.log('Axe') // your text
+console.log('Axe') // your text
+
+console.log('Axe') // your text
+console.log('Axe') // your text
+console.log('Axe') // your text
+
+console.log('Axe') // your text
+console.log('Axe') // your text
+console.log('Axe') // your text
