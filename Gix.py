6fdc3af08e945878b02759b451144c82a90dcefd
Lencof <71154457+Lencof@users.noreply.github.com>
2021-05-07 11:29:31 +0300

    Create Gix.py

---
+++ Gix.py
@@ -0,0 +1,41 @@
+# __Author__ __Lencof__
+# Gix.py
+
+import os
+import sys
+
+print('Gix') # your text
+print('Gix') # your text
+print('Gix') # your text
+
+print('Gix') # your text
+print('Gix') # your text
+print('Gix') # your text
+
+print('Gix') # your text
+print('Gix') # your text
+print('Gix') # your text
+
+print('Gix') # your text
+print('Gix') # your text
+print('Gix') # your text
+
+print('Gix') # your text
+print('Gix') # your text
+print('Gix') # your text
+
+print('Gix') # your text
+print('Gix') # your text
+print('Gix') # your text
+
+print('Gix') # your text
+print('Gix') # your text
+print('Gix') # your text
+
+print('Gix') # your text
+print('Gix') # your text
+print('Gix') # your text
+
+print('Gix') # your text
+print('Gix') # your text
+print('Gix') # your text
