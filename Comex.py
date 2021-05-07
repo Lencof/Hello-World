6fc0c945dd25319a8fa396eb4c5c8f6fdb8cf002
Lencof <71154457+Lencof@users.noreply.github.com>
2021-05-07 11:26:49 +0300

    Create Comex.py

---
+++ Comex.py
@@ -0,0 +1,41 @@
+# __Author__ __Lencof__
+# Comex.py
+
+import os
+import sys
+
+print('Comex') # your text
+print('Comex') # your text
+print('Comex') # your text
+
+print('Comex') # your text
+print('Comex') # your text
+print('Comex') # your text
+
+print('Comex') # your text
+print('Comex') # your text
+print('Comex') # your text
+
+print('Comex') # your text
+print('Comex') # your text
+print('Comex') # your text
+
+print('Comex') # your text
+print('Comex') # your text
+print('Comex') # your text
+
+print('Comex') # your text
+print('Comex') # your text
+print('Comex') # your text
+
+print('Comex') # your text
+print('Comex') # your text
+print('Comex') # your text
+
+print('Comex') # your text
+print('Comex') # your text
+print('Comex') # your text
+
+print('Comex') # your text
+print('Comex') # your text
+print('Comex') # your text
