a8d47690cac87224be5a9941e19f61496a2410a2
Lencof <71154457+Lencof@users.noreply.github.com>
2021-07-09 11:45:19 +0300

    Update Acar.js

--- Acar.js
+++ Acar.js
@@ -5,15 +5,15 @@ console.log('Acar')
 console.log('Acar') 
 console.log('Acar') 
 
-console.log('Acar') // your text
-console.log('Acar') // your text
-console.log('Acar') // your text
+console.log('Acar') 
+console.log('Acar') 
+console.log('Acar') 
 
-console.log('Acar') // your text
-console.log('Acar') // your text
-console.log('Acar') // your text
+console.log('Acar') 
+console.log('Acar') 
+console.log('Acar') 
 
-console.log('Acar') // your text
+console.log('Acar') 
 console.log('Acar') // your text
 console.log('Acar') // your text
 
