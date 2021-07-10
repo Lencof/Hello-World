82ee73d39167d990b1370bd0b8dec1abd1fd5c89
Lencof <71154457+Lencof@users.noreply.github.com>
2021-07-10 16:40:12 +0300

    Update Asena.js

--- Asena.js
+++ Asena.js
@@ -1,13 +1,13 @@
 // __Author__ __Lencof__
 // Asena.js
 
-console.log('Asena') // your text
-console.log('Asena') // your text
-console.log('Asena') // your text
+console.log('Asena') 
+console.log('Asena') 
+console.log('Asena') 
 
-console.log('Asena') // your text
-console.log('Asena') // your text
-console.log('Asena') // your text
+console.log('Asena') 
+console.log('Asena') 
+console.log('Asena') 
 
 console.log('Asena') // your text
 console.log('Asena') // your text
