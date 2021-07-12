478fb39132cc9d6a344c2c13d445017a0013fe43
Lencof <71154457+Lencof@users.noreply.github.com>
2021-07-12 03:23:27 +0300

    Update Axe.js

--- Axe.js
+++ Axe.js
@@ -1,13 +1,13 @@
 // __Author__ __Lencof__
 // Axe.js
 
-console.log('Axe') // your text
-console.log('Axe') // your text
-console.log('Axe') // your text
+console.log('Axe') 
+console.log('Axe') 
+console.log('Axe') 
 
-console.log('Axe') // your text
-console.log('Axe') // your text
-console.log('Axe') // your text
+console.log('Axe') 
+console.log('Axe') 
+console.log('Axe') 
 
 console.log('Axe') // your text
 console.log('Axe') // your text
