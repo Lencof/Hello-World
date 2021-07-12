2d1cd92d4c9cd2e3b9384b29e32af04e1137ed9f
Lencof <71154457+Lencof@users.noreply.github.com>
2021-07-12 03:44:02 +0300

    Update Bast.js

--- Bast.js
+++ Bast.js
@@ -1,21 +1,21 @@
 // __Author__ __Lencof__
 // Bast.js
 
-cosole.log('Bast') // your text
-cosole.log('Bast') // your text
-cosole.log('Bast') // your text
+cosole.log('Bast') 
+cosole.log('Bast') 
+cosole.log('Bast') 
 
-cosole.log('Bast') // your text
-cosole.log('Bast') // your text
-cosole.log('Bast') // your text
+cosole.log('Bast')
+cosole.log('Bast') 
+cosole.log('Bast') 
 
-cosole.log('Bast') // your text
-cosole.log('Bast') // your text
-cosole.log('Bast') // your text
+cosole.log('Bast') 
+cosole.log('Bast') 
+cosole.log('Bast') 
 
-cosole.log('Bast') // your text
-cosole.log('Bast') // your text
-cosole.log('Bast') // your text
+cosole.log('Bast') 
+cosole.log('Bast') 
+cosole.log('Bast') 
 
 cosole.log('Bast') // your text
 cosole.log('Bast') // your text
