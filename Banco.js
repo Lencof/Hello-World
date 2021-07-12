210482bd5a7f4c1185b04e000cb63d9d92274df9
Lencof <71154457+Lencof@users.noreply.github.com>
2021-07-12 03:34:16 +0300

    Update Banco.js

--- Banco.js
+++ Banco.js
@@ -1,13 +1,13 @@
 // __Author__ __Lencof__
 // Banco.js
 
-console.log('Banco') // your text
-console.log('Banco') // your text
-console.log('Banco') // your text
+console.log('Banco') 
+console.log('Banco') 
+console.log('Banco') 
 
-console.log('Banco') // your text
-console.log('Banco') // your text
-console.log('Banco') // your text
+console.log('Banco') 
+console.log('Banco') 
+console.log('Banco') 
 
 console.log('Banco') // your text
 console.log('Banco') // your text
