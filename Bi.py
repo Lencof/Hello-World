e2364a017d1ed2fdb6771b5945eb4b76afdf9fbc
Lencof <71154457+Lencof@users.noreply.github.com>
2021-07-12 03:56:28 +0300

    Update Bi.py

--- Bi.py
+++ Bi.py
@@ -1,13 +1,13 @@
 # __Author__ __Lencof__
 # Bi.py
 
-print('Bi') # your text
-print('Bi') # your text
-print('Bi') # your text
+print('Bi') 
+print('Bi') 
+print('Bi') 
 
-print('Bi') # your text
-print('Bi') # your text
-print('Bi') # your text
+print('Bi')
+print('Bi') 
+print('Bi') 
 
 print('Bi') # your text
 print('Bi') # your text
