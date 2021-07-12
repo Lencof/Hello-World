604697825a9a3819696ded47998903bad7c5ec37
Lencof <71154457+Lencof@users.noreply.github.com>
2021-07-12 03:25:55 +0300

    Update BAX.py

--- BAX.py
+++ BAX.py
@@ -9,9 +9,9 @@ print('BAX')
 print('BAX') 
 print('BAX') 
 
-print('BAX') # your text
-print('BAX') # your text
-print('BAX') # your text
+print('BAX')
+print('BAX') 
+print('BAX') 
 
 print('BAX') # your text
 print('BAX') # your text
