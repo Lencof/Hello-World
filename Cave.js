dfdbcc385bf0a4b62145da4aff9cb7b8fb891b70
Lencof <71154457+Lencof@users.noreply.github.com>
2021-07-12 04:02:25 +0300

    Update Cave.js

--- Cave.js
+++ Cave.js
@@ -1,13 +1,13 @@
 // __Author__ __Lencof__
 // Cave.js
 
-console.log('Cave') // your text
-console.log('Cave') // your text
-console.log('Cave') // your text
+console.log('Cave') 
+console.log('Cave') 
+console.log('Cave') 
 
-console.log('Cave') // your text
-console.log('Cave') // your text
-console.log('Cave') // your text
+console.log('Cave') 
+console.log('Cave') 
+console.log('Cave') 
 
 console.log('Cave') // your text
 console.log('Cave') // your text
