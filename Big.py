2ff5faca38f8770bf05918d931511ec1ca22afef
Lencof <71154457+Lencof@users.noreply.github.com>
2021-07-12 03:58:12 +0300

    Update Big.py

--- Big.py
+++ Big.py
@@ -1,13 +1,13 @@
 # __Author__ __Lencof__
 # Big.py
 
-print('Bigs') # your text
-print('Bigs') # your text
-print('Bigs') # your text
+print('Bigs') 
+print('Bigs')
+print('Bigs') 
 
-print('Bigs') # your text
-print('Bigs') # your text
-print('Bigs') # your text
+print('Bigs') 
+print('Bigs') 
+print('Bigs') 
 
 print('Bigs') # your text
 print('Bigs') # your text
