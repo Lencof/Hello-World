31ff63327674ee44e95f9388d7d89d45cbe0d1ab
Lencof <71154457+Lencof@users.noreply.github.com>
2021-07-13 10:51:08 +0300

    Update CerNel.py

--- CerNel.py
+++ CerNel.py
@@ -1,13 +1,13 @@
 # __Author__ __Lencof__
 # CerNel.py
 
-print('CerNel') # your text
-print('CerNel') # your text
-print('CerNel') # your text
+print('CerNel') 
+print('CerNel') 
+print('CerNel') 
 
-print('CerNel') # your text
-print('CerNel') # your text
-print('CerNel') # your text
+print('CerNel') 
+print('CerNel') 
+print('CerNel') 
 
 print('CerNel') # your text
 print('CerNel') # your text
