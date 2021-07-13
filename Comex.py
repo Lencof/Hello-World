f66fbcf5f25c4bbb3a4724957b18aca7977e7c07
Lencof <71154457+Lencof@users.noreply.github.com>
2021-07-13 10:56:14 +0300

    Update Comex.py

--- Comex.py
+++ Comex.py
@@ -9,9 +9,9 @@ print('Comex')
 print('Comex') 
 print('Comex') 
 
-print('Comex') # your text
-print('Comex') # your text
-print('Comex') # your text
+print('Comex') 
+print('Comex') 
+print('Comex') 
 
 print('Comex') # your text
 print('Comex') # your text
