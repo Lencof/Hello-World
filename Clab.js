460bbdc66a265e1b23ebdc20617b3d09043a9d77
Lencof <71154457+Lencof@users.noreply.github.com>
2021-07-13 10:52:55 +0300

    Update Clab.js

--- Clab.js
+++ Clab.js
@@ -1,13 +1,13 @@
 // __Author__ __Lencof__
 // Clab.js
 
-console.log('Clab') // your text
-console.log('Clab') // your text
-console.log('Clab') // your text
+console.log('Clab') 
+console.log('Clab') 
+console.log('Clab') 
 
-console.log('Clab') // your text
-console.log('Clab') // your text
-console.log('Clab') // your text
+console.log('Clab') 
+console.log('Clab') 
+console.log('Clab') 
 
 console.log('Clab') // your text
 console.log('Clab') // your text
