f39204d9f0a161daa12ba145fcfebddfecc01ccc
Lencof <71154457+Lencof@users.noreply.github.com>
2021-07-14 08:53:23 +0300

    Update CountryS.js

--- CountryS.js
+++ CountryS.js
@@ -21,9 +21,9 @@ console.log('CountryS')
 console.log('CountryS') 
 console.log('CountryS') 
 
-console.log('CountryS') // your text
-console.log('CountryS') // your text
-console.log('CountryS') // your text
+console.log('CountryS') 
+console.log('CountryS') 
+console.log('CountryS') 
 
 console.log('CountryS') // your text
 console.log('CountryS') // your text
