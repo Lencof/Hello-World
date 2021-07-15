e7b1d9c4c9afae901213d1753b810f151ac3b5b2
Lencof <71154457+Lencof@users.noreply.github.com>
2021-07-15 08:32:21 +0300

    Update Deron.js

--- Deron.js
+++ Deron.js
@@ -1,9 +1,9 @@
 // __Author__ __Lencof__
 // Deron.js
 
-console.log('Deron') // your text
-console.log('Deron') // your text
-console.log('Deron') // your text
+console.log('Deron')
+console.log('Deron') 
+console.log('Deron') 
 
 console.log('Deron') // your text
 console.log('Deron') // your text
