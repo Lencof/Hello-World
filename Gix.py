4dcf6c308bfe9eb1da56b7e842a839888c94a33a
Lencof <71154457+Lencof@users.noreply.github.com>
2021-07-16 03:38:56 +0300

    Update Gix.py

--- Gix.py
+++ Gix.py
@@ -9,9 +9,9 @@ print('Gix')
 print('Gix') 
 print('Gix') 
 
-print('Gix') # your text
-print('Gix') # your text
-print('Gix') # your text
+print('Gix') 
+print('Gix') 
+print('Gix') 
 
 print('Gix') # your text
 print('Gix') # your text
