ed31f65d46116a63c60eaa06bd0a0be5db6a8202
Lencof <71154457+Lencof@users.noreply.github.com>
2021-07-17 08:26:02 +0300

    Update Gola.js

--- Gola.js
+++ Gola.js
@@ -25,9 +25,9 @@ console.log('Gola')
 console.log('Gola') 
 console.log('Gola') 
 
-console.log('Gola') // your text
-console.log('Gola') // your text
-console.log('Gola') // your text
+console.log('Gola') 
+console.log('Gola') 
+console.log('Gola') 
 
 console.log('Gola') // your text
 console.log('Gola') // your text
