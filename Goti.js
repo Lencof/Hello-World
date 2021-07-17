45be32d2e36e3cc6f776f6197311d398e414e325
Lencof <71154457+Lencof@users.noreply.github.com>
2021-07-17 13:17:26 +0300

    Update Goti.js

--- Goti.js
+++ Goti.js
@@ -17,9 +17,9 @@ console.log('Goti')
 console.log('Goti') 
 console.log('Goti') 
 
-console.log('Goti') // your text
-console.log('Goti') // your text
-console.log('Goti') // your text
+console.log('Goti') 
+console.log('Goti') 
+console.log('Goti') 
 
 console.log('Goti') // your text
 console.log('Goti') // your text
