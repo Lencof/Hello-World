3e2e5c134c7a95682f0a8a2ae25efd1dbcd0606f
Lencof <71154457+Lencof@users.noreply.github.com>
2021-07-17 13:18:50 +0300

    Update Guza.js

--- Guza.js
+++ Guza.js
@@ -1,11 +1,11 @@
 // __Author__ __Lencof__
 // Guza.js
 
-console.log('Guza') // your text
-console.log('Guza') // your text
-console.log('Guza') // your text
+console.log('Guza') 
+console.log('Guza') 
+console.log('Guza') 
 
-console.log('Guza') // your text
+console.log('Guza') 
 console.log('Guza') // your text
 console.log('Guza') // your text
 
