f080d870005f60c88befaf781d63dc9aba1f98bd
Lencof <71154457+Lencof@users.noreply.github.com>
2021-07-18 08:42:17 +0300

    Update Hello_World.js

--- Hello_World.js
+++ Hello_World.js
@@ -1,9 +1,9 @@
 // __Author__ __Lencof__
 // Hello_World.js
 
-console.log("Hello World!") // your text
-console.log("Hello World!") // your text
-console.log("Hello World!") // your text
+console.log("Hello World!") 
+console.log("Hello World!") 
+console.log("Hello World!") 
 
 console.log("Hello World!") // your text
 console.log("Hello World!") // your text
