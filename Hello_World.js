2afcc87bb22ac291b0d161e6dca79209763ebec9
Lencof <71154457+Lencof@users.noreply.github.com>
2021-07-18 08:42:27 +0300

    Update Hello_World.js

--- Hello_World.js
+++ Hello_World.js
@@ -5,9 +5,9 @@ console.log("Hello World!")
 console.log("Hello World!") 
 console.log("Hello World!") 
 
-console.log("Hello World!") // your text
-console.log("Hello World!") // your text
-console.log("Hello World!") // your text
+console.log("Hello World!") 
+console.log("Hello World!") 
+console.log("Hello World!") 
 
 console.log("Hello World!") // your text
 console.log("Hello World!") // your text
