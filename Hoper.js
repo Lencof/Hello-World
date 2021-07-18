adacc64196d06d6ef1a5eec75b6539a8e1fd450a
Lencof <71154457+Lencof@users.noreply.github.com>
2021-07-18 08:45:01 +0300

    Update Hoper.js

--- Hoper.js
+++ Hoper.js
@@ -5,9 +5,9 @@ console.log('Hoper')
 console.log('Hoper') 
 console.log('Hoper') 
 
-console.log('Hoper') // your text
-console.log('Hoper') // your text
-console.log('Hoper') // your text
+console.log('Hoper') 
+console.log('Hoper')
+console.log('Hoper') 
 
 console.log('Hoper') // your text
 console.log('Hoper') // your text
