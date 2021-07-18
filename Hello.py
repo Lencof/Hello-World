8a575c5d8c0a3dfa5b6efc1402254a0f932e705d
Lencof <71154457+Lencof@users.noreply.github.com>
2021-07-18 08:40:36 +0300

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -2,9 +2,9 @@
 # Hello.py
 # using print to display text
 
-print('Hello Worlds') # your text
-print('Hello Worlds') # your text
-print('Hello Worlds') # your text
+print('Hello Worlds') 
+print('Hello Worlds') 
+print('Hello Worlds') 
 
 print("Hello Worlds") # your text
 print("Hello Worlds") # your text
