1c242b5f40b8ce85b855ccfd6eaa88c3692df335
Lencof <71154457+Lencof@users.noreply.github.com>
2021-07-18 08:49:40 +0300

    Update Lopez.py

--- Lopez.py
+++ Lopez.py
@@ -1,9 +1,9 @@
 # __Author__ __Lencof__
 # Lopez.py
 
-print('Hi! Lopez') # your text
-print('Hi! Lopez') # your text
-print('Hi! Lopez') # your text
+print('Hi! Lopez') 
+print('Hi! Lopez') 
+print('Hi! Lopez') 
 
 print('Hi! Lopez') # your text
 print('Hi! Lopez') # your text
