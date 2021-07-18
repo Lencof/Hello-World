3397cf6e0f0cd5389e6cdb384dcac16f1659a28c
Lencof <71154457+Lencof@users.noreply.github.com>
2021-07-18 08:51:54 +0300

    Update Lopez.py

--- Lopez.py
+++ Lopez.py
@@ -33,6 +33,6 @@ print('Hi! Lopez')
 print('Hi! Lopez') 
 print('Hi! Lopez') 
 
-print('Hi! Lopez') # your text
-print('Hi! Lopez') # your text
-print('Hi! Lopez') # your text
+print('Hi! Lopez') 
+print('Hi! Lopez') 
+print('Hi! Lopez') 
