bdef07a98afa4988c704328bd6101a1a433bc406
Lencof <71154457+Lencof@users.noreply.github.com>
2021-07-19 04:34:21 +0300

    Update Mendos.js

--- Mendos.js
+++ Mendos.js
@@ -9,9 +9,9 @@ console.log('Mendos')
 console.log('Mendos') 
 console.log('Mendos')
 
-console.log('Mendos') // your text
-console.log('Mendos') // your text
-console.log('Mendos') // your text
+console.log('Mendos') 
+console.log('Mendos') 
+console.log('Mendos') 
 
 console.log('Mendos') // your text
 console.log('Mendos') // your text
