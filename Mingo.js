e05c83c50a4026d003cc297805012cd2b8a654f7
Lencof <71154457+Lencof@users.noreply.github.com>
2021-07-19 04:35:43 +0300

    Update Mingo.js

--- Mingo.js
+++ Mingo.js
@@ -1,13 +1,13 @@
 // __Author__ __Lencof__
 // Mingo.js
 
-console.log('Mingo') // your text
-console.log('Mingo') // your text
-console.log('Mingo') // your text
+console.log('Mingo') 
+console.log('Mingo') 
+console.log('Mingo') 
 
-console.log('Mingo') // your text
-console.log('Mingo') // your text
-console.log('Mingo') // your text
+console.log('Mingo') 
+console.log('Mingo') 
+console.log('Mingo') 
 
 console.log('Mingo') // your text
 console.log('Mingo') // your text
