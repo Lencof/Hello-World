3c2aa3da6bc734477f10f716c87350b4271f4c96
Lencof <71154457+Lencof@users.noreply.github.com>
2021-07-19 04:38:06 +0300

    Update NO.py

--- NO.py
+++ NO.py
@@ -21,9 +21,9 @@ print('NO')
 print('NO') 
 print('NO') 
 
-print('NO') # your text
-print('NO') # your text
-print('NO') # your text
+print('NO') 
+print('NO') 
+print('NO') 
 
 print('NO') # your text
 print('NO') # your text
