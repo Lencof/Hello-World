1b8d1213ade08836aa54de1d5337e87e8d6e7a40
Lencof <71154457+Lencof@users.noreply.github.com>
2021-07-20 03:58:16 +0300

    Update Paymer.js

--- Paymer.js
+++ Paymer.js
@@ -21,9 +21,9 @@ console.log('Paymer')
 console.log('Paymer') 
 console.log('Paymer') 
 
-console.log('Paymer') // your text
-console.log('Paymer') // your text
-console.log('Paymer') // your text
+console.log('Paymer') 
+console.log('Paymer') 
+console.log('Paymer') 
 
 console.log('Paymer') // your text
 console.log('Paymer') // your text
