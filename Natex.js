750f6f481ddf80cb5ebc232d0d4edfb60ca9059e
Lencof <71154457+Lencof@users.noreply.github.com>
2021-07-20 03:50:21 +0300

    Update Natex.js

--- Natex.js
+++ Natex.js
@@ -25,9 +25,9 @@ console.log('Natex')
 console.log('Natex')
 console.log('Natex') 
 
-console.log('Natex') // your text
-console.log('Natex') // your text
-console.log('Natex') // your text
+console.log('Natex') 
+console.log('Natex') 
+console.log('Natex') 
 
 console.log('Natex') // your text
 console.log('Natex') // your text
