aaa932a705255cffeef7f1cf902c6a7b29de84f4
Lencof <71154457+Lencof@users.noreply.github.com>
2021-07-21 09:18:02 +0300

    Update Quetes.js

--- Quetes.js
+++ Quetes.js
@@ -25,9 +25,9 @@ console.log('Quetes')
 console.log('Quetes') 
 console.log('Quetes') 
 
-console.log('Quetes') // your text
-console.log('Quetes') // your text
-console.log('Quetes') // your text
+console.log('Quetes') 
+console.log('Quetes') 
+console.log('Quetes') 
 
 console.log('Quetes') // your text
 console.log('Quetes') // your text
