951026ae085b28842999c87180cc6b5acd7479d4
Lencof <71154457+Lencof@users.noreply.github.com>
2021-07-21 09:19:23 +0300

    Update Rebol.js

--- Rebol.js
+++ Rebol.js
@@ -13,9 +13,9 @@ console.log('Rebol')
 console.log('Rebol') 
 console.log('Rebol')
 
-console.log('Rebol') // your text
-console.log('Rebol') // your text
-console.log('Rebol') // your text
+console.log('Rebol') 
+console.log('Rebol') 
+console.log('Rebol') 
 
 console.log('Rebol') // your text
 console.log('Rebol') // your text
