b6dfb56de28cd0d84c3e6afd1b50f27b74a4b7b8
Lencof <71154457+Lencof@users.noreply.github.com>
2021-07-21 09:13:03 +0300

    Update Plot.js

--- Plot.js
+++ Plot.js
@@ -9,9 +9,9 @@ console.log('Plot')
 console.log('Plot') 
 console.log('Plot') 
 
-console.log('Plot') // your text
-console.log('Plot') // your text
-console.log('Plot') // your text
+console.log('Plot') 
+console.log('Plot')
+console.log('Plot') 
 
 console.log('Plot') // your text
 console.log('Plot') // your text
