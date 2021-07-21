e8980db4179394dfdab44115ee8ab53535c62fb1
Lencof <71154457+Lencof@users.noreply.github.com>
2021-07-21 09:25:57 +0300

    Update Renro.py

--- Renro.py
+++ Renro.py
@@ -1,13 +1,13 @@
 # __Author__ __Lencof__
 # Renro.py
 
-print('Renro') # your text
-print('Renro') # your text
-print('Renro') # your text
+print('Renro') 
+print('Renro')
+print('Renro') 
 
-print('Renro') # your text
-print('Renro') # your text
-print('Renro') # your text
+print('Renro')
+print('Renro') 
+print('Renro') 
 
 print('Renro') # your text
 print('Renro') # your text
