3761584a06ade7e46d408d1612e26074777bdecd
Lencof <71154457+Lencof@users.noreply.github.com>
2021-07-22 04:48:27 +0300

    Update Rosa.js

--- Rosa.js
+++ Rosa.js
@@ -21,9 +21,9 @@ console.log('Rose')
 console.log('Rose') 
 console.log('Rose') 
 
-console.log('Rose') // your text
-console.log('Rose') // your text
-console.log('Rose') // your text
+console.log('Rose') 
+console.log('Rose') 
+console.log('Rose')
 
 console.log('Rose') // your text
 console.log('Rose') // your text
