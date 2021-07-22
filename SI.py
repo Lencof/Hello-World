512abb7c5f00e9acad1b774214ad8e27b49365ca
Lencof <71154457+Lencof@users.noreply.github.com>
2021-07-22 04:51:07 +0300

    Update SI.py

--- SI.py
+++ SI.py
@@ -17,9 +17,9 @@ print('SI')
 print('SI') 
 print('SI') 
 
-print('SI') # your text
-print('SI') # your text
-print('SI') # your text
+print('SI') 
+print('SI') 
+print('SI') 
 
 print('SI') # your text
 print('SI') # your text
