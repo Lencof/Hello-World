8ffe20ac270b1f7f7a72060446d49a578597d6b1
Lencof <71154457+Lencof@users.noreply.github.com>
2021-07-24 02:32:41 +0300

    Update Z.py

--- Z.py
+++ Z.py
@@ -21,9 +21,9 @@ print('Z')
 print('Z')
 print('Z') 
 
-print('Z') # your text
-print('Z') # your text
-print('Z') # your text
+print('Z') 
+print('Z') 
+print('Z') 
 
 print('Z') # your text
 print('Z') # your text
