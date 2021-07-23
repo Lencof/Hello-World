0620bdddaf1fb392c2bd053c62a094b1a3dd5894
Lencof <71154457+Lencof@users.noreply.github.com>
2021-07-23 16:49:42 +0300

    Update Vacax.js

--- Vacax.js
+++ Vacax.js
@@ -29,9 +29,9 @@ console.log('Vacax')
 console.log('Vacax') 
 console.log('Vacax') 
 
-console.log('Vacax') // your text
-console.log('Vacax') // your text
-console.log('Vacax') // your text
+console.log('Vacax') 
+console.log('Vacax') 
+console.log('Vacax') 
 
 console.log('Vacax') // your text
 console.log('Vacax') // your text
