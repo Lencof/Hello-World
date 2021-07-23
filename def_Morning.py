cd221d1c3c4e3e69d3e03c43812c7d91dc8f1e3f
Lencof <71154457+Lencof@users.noreply.github.com>
2021-07-24 02:42:01 +0300

    Update def_Morning.py

--- def_Morning.py
+++ def_Morning.py
@@ -1,10 +1,10 @@
 # __Author__ __Lencof__
 # def_Morning.py
 
-def Morning(): # create def Morning():
-    print('Morning') # your text
-    print('Morning') # your text
-    print('Morning') # your text
+def Morning(): 
+    print('Morning') 
+    print('Morning') 
+    print('Morning') 
     
     print('Morning') # your text
     print('Morning') # your text
