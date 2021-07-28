a84facf1ecd741eb28ca29fce655771e54e92b13
Lencof <71154457+Lencof@users.noreply.github.com>
2021-07-28 04:47:59 +0300

    Update Sole.js

--- Sole.js
+++ Sole.js
@@ -21,9 +21,9 @@ console.log('Sole')
 console.log('Sole') 
 console.log('Sole') 
 
-console.log('Sole') // your text
-console.log('Sole') // your text
-console.log('Sole') // your text
+console.log('Sole') 
+console.log('Sole') 
+console.log('Sole') 
 
 console.log('Sole') // your text
 console.log('Sole') // your text
