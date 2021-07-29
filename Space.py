0209dcf975ee3f22a533f37975fd047bd7c53aed
Lencof <71154457+Lencof@users.noreply.github.com>
2021-07-29 05:49:48 +0300

    Update Space.py

--- Space.py
+++ Space.py
@@ -25,9 +25,9 @@ print('Space')
 print('Space') 
 print('Space') 
 
-print('Space') # your text
-print('Space') # your text
-print('Space') # your text
+print('Space') 
+print('Space') 
+print('Space')
 
 print('Space') # your text
 print('Space') # your text
